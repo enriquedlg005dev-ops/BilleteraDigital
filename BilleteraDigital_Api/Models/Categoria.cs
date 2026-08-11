212c94d730c9f669330b0b02976780571b407048
KIKE <Enriquedlg005.dev@gmail.com>
2026-08-10 22:32:39 -0500

    Se agregaron los modelos de Usuario y Categoria

---
+++ BilleteraDigital_Api/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿namespace BilleteraDigital_Api.Models
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public int? IdUsuario { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string? Descripcion { get; set; }
+
+        public bool Estado { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+    }
+}
---
+++ BilleteraDigital_Api/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BilleteraDigital_Api.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Apellido { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
+
+        public string Contrasena { get; set; } = string.Empty;
+
+        public string? Telefono { get; set; }
+
+        public bool Estado { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+    }
+}
